70908dc5ef00a3de5e1e8813625ccdf2cd411dc1
Engkos <engkoskostaman759@gmail.com>
2023-02-05 00:02:29 +0700

    function components 1 file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React,{Component} from 'react'
 import Header from './Header';
+
+const Footer = () => {
+  return(
+    <div>
+      <h3>halaman 3</h3>
+      <p>2023</p>
+    </div>
+  );
+}
 class App extends Component {
   render(){
     return (
      <div>
       <Header/>
       <h1>Rendering Elements</h1>
+     <Footer/>
      </div>
     );
   }

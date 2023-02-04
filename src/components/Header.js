d5cc8d704a42c2b41435f05a14656298cf576983
Engkos <engkoskostaman759@gmail.com>
2023-02-05 03:31:12 +0700

    state components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,17 @@
 import React,{Component} from 'react'
 
 class Header extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      daftar : "daftar makanan nasional"
+    }
+  }
   render(){
     return (
      <div>
       <h1> Elements</h1>
+      <h2>{this.state.daftar}</h2>
      </div>
     );
   }

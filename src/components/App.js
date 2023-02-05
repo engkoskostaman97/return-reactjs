441ef13d9932955ce8dcb6a1a3406adbde8c0705
Engkos <engkoskostaman759@gmail.com>
2023-02-05 15:15:26 +0700

    state and props

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render(){
     return (
      <div>
-      <Header/>
+      <Header list="3 terbaik" />
       <List/>
      <Footer name="makanan nusantara" tahun ="2023"/>
      </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,8 @@ class Header extends Component {
   constructor(props){
     super(props);
     this.state={
-      daftar : "daftar makanan nasional"
+      daftar : "daftar makanan nasional",
+      daftarlist: this.props.list
     }
   }
   render(){
@@ -12,6 +13,7 @@ class Header extends Component {
      <div>
       <h1> Elements</h1>
       <h2>{this.state.daftar}</h2>
+      <p>{this.state.daftarlist}</p>
      </div>
     );
   }
